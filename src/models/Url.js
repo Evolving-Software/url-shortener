a46a439b2fe52f81bf8c8b052708f5a3d06efb49
Riley Seaburg <riley.seaburg@22enterprises.com>
2022-03-10 10:01:53 -0500

    Begin Modeling new URL

--- src/models/Url.js
+++ src/models/Url.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+// Todo: Add Label and Description
+
+
 const urlSchema = new Schema({
     longUrl: String,
     shortUrl: String,
     urlCode: String,
-    date: Date
+    date: Date,
+    label: String,
+    description: String
 });
 
 module.exports = mongoose.models.Url || mongoose.model('Url', urlSchema);
